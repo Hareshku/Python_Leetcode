54067667b7d696f9774bc105fbf35c05a9af7341
Hareshku <hareesh7737@gmail.com>
2025-09-19 00:18:08 +0500

    Python revision day one

---
+++ day1/day1.py
@@ -0,0 +1,10 @@
+name ="Haresh"
+rollNo="21SW053"
+department="Software Engineering"
+
+print("name =",name, "\nrollNo =",rollNo, "\ndepartment =", department)
+
+name= input("Enter your name =")
+age= int(input("Enter you age  ="))
+
+print("Name =",name, "\nage =", age)
